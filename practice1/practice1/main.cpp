0a0a38c84d38e065141c57cdb0f7fa6df448cdfa
P1ckUp <duck7614477@gmail.com>
2026-05-07 19:02:26 +0900

    Update main.cpp

--- practice1/practice1/main.cpp
+++ practice1/practice1/main.cpp
@@ -51,6 +51,7 @@ int main()
 //사용자로부터 0보다 큰 정수들의 목록을 입력받는 프로그램을 작성하시오.
 //사용자가 종료를 의미하는 값인 0을 입력할 때까지 계속 입력받는다.
 //종료 값(sentinel)인 0이 입력되면, 프로그램은 입력된 값들 중에서 가장 큰 값을 출력해야 한다.
+/*
 #include<cstdio>
 #include<iostream>
 int main()
@@ -60,10 +61,11 @@ int main()
 
 	while (true)
 	{
-		std::cout << "값을 입력하세요.: ";
+		std::cout << "enter the value: ";
 		std::cin >> a;
 		if (a == 0)
 		{
+			std::cout << "program is finished." << std::endl;
 			break;
 		}
 		else if (a > max)
@@ -75,12 +77,57 @@ int main()
 
 return 0;
 }
+*/
+
+//컴파일 될 수 있도록 코드를 작성 및 수정
+//성공적으로 프린트(45가 나와야됨)
+#include<cstdio>
+#include<iostream>
+
+int accumulate(int arr[], int size)
+{
+	int Sum = 0;
+
+	for (int i = 0; i < size; ++i)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
+
+float accumulate(float arr[], int size)
+{
+	float Sum = 0.00f;
+
+	for (int i = 0; i < size; ++i)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
 
 
+int main(void)
 
 
+{
+	const int size = 9;
+	int iNums[size] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	float fNums[size] = {};
 
+	for (int i = 0; i < size; ++i)
+	{
+		fNums[i] = iNums[i] + 0.1f * (float)(i + 1);
+	}
 
+	// Todo1: accumulate 함수 구현
+	int iSum = accumulate(iNums, size);
+	float fSum = accumulate(fNums, size);
 
+	// Todo2: 출력 iSum, fSum
 
+	std::cout << "iSum: " << iSum << std::endl;
+	std::cout << "fSum: " << fSum << std::endl;
 
+	return 0;
+}
