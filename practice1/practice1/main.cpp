bdbe06b2c8a36ba39ea8d67a38a84c6b653adfbf
P1ckUp <duck7614477@gmail.com>
2026-05-07 16:39:06 +0900

    Update main.cpp

--- practice1/practice1/main.cpp
+++ practice1/practice1/main.cpp
@@ -2,10 +2,16 @@
 
 int main(int agrc, char** argv)
 {
-	using namespace std;
 
-	printf("Hello, World!\n"); // C-style output
+//	printf("Hello, World!\n"); // C-style output
 
-	std::cout << "Hello, World!"; // C++-style output, more type-safe and extensible
+	std::cout << "당신의 나이는 몇 살인가요? "; // C++-style output
+
+	int age = 0;
+
+	std::cin >> age; // C++-style input, more type-safe and extensible
+	
+	std::cout << "당신의 나이는 " << age << "살입니다." << std::endl; // C++-style output, more type-safe and extensible
+
 	return 0;
 }
