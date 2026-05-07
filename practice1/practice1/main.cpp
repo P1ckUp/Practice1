c2134fd436a96b43506e11425cd18cd79c727e03
P1ckUp <duck7614477@gmail.com>
2026-05-07 17:29:36 +0900

    reference 1

--- practice1/practice1/main.cpp
+++ practice1/practice1/main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<string>
-
+/*
 int main(int agrc, char** argv)
 {
+	
 	// 이름을 저장하기 위한 c++ 변수 자료형 = std::string
 	// concatination = std::string + std::string
 	// concat => 새로운 문자열 = 문자열1 + 문자열2
@@ -15,6 +16,33 @@ int main(int agrc, char** argv)
 	std::string fullname = firstname + " " + lastname;
 
 	std::cout << "English Name: " << fullname << std::endl;
+	*/
+// =======================================================
+/*
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	std::cout<<"before swap: a = "<<a<<", b = "<<b<<std::endl;
+}
+*/
+int main()
+{
+	int age = 10;
+
+	int& ref = age;
+
+	ref = 20;
+
+	std::cout << "age: " << age << std::endl;
 
 	return 0;
 }
