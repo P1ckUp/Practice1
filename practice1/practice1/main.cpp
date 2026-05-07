14303c3c94e32eedd7457b2cec4d15c43711f940
P1ckUp <duck7614477@gmail.com>
2026-05-07 16:45:38 +0900

    string practice 1

--- practice1/practice1/main.cpp
+++ practice1/practice1/main.cpp
@@ -7,6 +7,6 @@ int main(int agrc, char** argv)
 	std::string name = "이동훈";
 	std::string english_name = "Lee Dong Hoon";
 
-	std::cout << "영문: " << english_name << std::endl;
-	std::cout << "한글: " << name << std::endl;
+	std::cout << "ENG NAME: " << english_name << std::endl;
+	std::cout << "KOR NAME: " << name << std::endl;
 }
