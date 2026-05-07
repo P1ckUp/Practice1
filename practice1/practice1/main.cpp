0f3b90a01d502c5c361c86e659e721413f9adba7
P1ckUp <duck7614477@gmail.com>
2026-05-07 16:44:35 +0900

    Update main.cpp

--- practice1/practice1/main.cpp
+++ practice1/practice1/main.cpp
@@ -1,17 +1,12 @@
 #include<iostream>
+#include<string>
 
 int main(int agrc, char** argv)
 {
+	// 이름을 저장하기 위한 c++ 변수 자료형 = std::string
+	std::string name = "이동훈";
+	std::string english_name = "Lee Dong Hoon";
 
-//	printf("Hello, World!\n"); // C-style output
-
-	std::cout << "당신의 나이는 몇 살인가요? "; // C++-style output
-
-	int age = 0;
-
-	std::cin >> age; // C++-style input, more type-safe and extensible
-	
-	std::cout << "당신의 나이는 " << age << "살입니다." << std::endl; // C++-style output, more type-safe and extensible
-
-	return 0;
+	std::cout << "영문: " << english_name << std::endl;
+	std::cout << "한글: " << name << std::endl;
 }
